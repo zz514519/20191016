b5c80b3bff9e00db70804f6689d5248bc2cf2bb7
zz514519 <1475430017@qq.com>
2019-10-17 18:31:07 +0800

    练习

---
+++ 20190926/src/Test1016a.java
@@ -0,0 +1,12 @@
+//类和对象。
+/public class Test1016a{
+        public static void main(String[] args){
+            class Person{
+                public String name ="张三";
+                public int age = 18;
+            }
+            Person person = new Person();
+            System.out.println(person.name+" "+person.age);
+        }
+    }
+
